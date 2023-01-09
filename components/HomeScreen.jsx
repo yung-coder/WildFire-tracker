fb53987e9936dc42ad8c3d1c9e23b88bcf18763b
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-09 23:12:10 +0530

    styling

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -4,24 +4,26 @@ import { staggerContainer, textVariant } from "../utils/motions";
 
 const HomeScreen = () => {
   return (
-    <section className="flex justify-center items-center border sm:py-[20%] py-40 xs:py-8  sm:pl-16 pl-6">
-      <motion.div
-        variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className="flex flex-col justify-between items-center"
-      >
-        <div className="flex justify-center items-center ">
-          <motion.h1
-            variants={textVariant(1.1)}
-            className="font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white"
-          >
-            Welcome
-          </motion.h1>
-        </div>
-      </motion.div>
-    </section>
+    <>
+      <section className="flex justify-center items-center sm:py-[15%] py-40 xs:py-8  sm:pl-16 pl-6">
+        <motion.div
+          variants={staggerContainer}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.25 }}
+          className="flex flex-col justify-between items-center"
+        >
+          <div className="flex justify-center items-center ">
+            <motion.h1
+              variants={textVariant(1.1)}
+              className="font-bold lg:text-[70px] md:text-[80px] sm:text-[30px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white"
+            >
+              Welcome
+            </motion.h1>
+          </div>
+        </motion.div>
+      </section>
+    </>
   );
 };
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import HomeScreen from "../components/HomeScreen";
 function App() {
   return (
-    <div className="">
-      <HomeScreen />
+    <div>
+      <div className="relative">
+        <HomeScreen />
+        <div className="absolute w-[100%] bottom-[60%] right-45 inset-0 gradient-01" />
+      </div>
     </div>
   );
 }
